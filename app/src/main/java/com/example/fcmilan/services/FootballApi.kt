7e170d61dbc4e3e2a068d16ec1489d91eba1708a
marshallmutiso <marshallmusyimi@gmail.com>
2023-05-31 17:53:15 +0300

    Add api key to buildconfig.

--- app/src/main/java/com/example/fcmilan/services/FootballApi.kt
+++ app/src/main/java/com/example/fcmilan/services/FootballApi.kt
@@ -1,5 +1,8 @@
 package com.example.fcmilan.services
 
+import com.example.fcmilan.BuildConfig
+
 class FootballApi {
     val BASE_URL = "https://v3.football.api-sports.io/"
+    val API_KEY = BuildConfig.API_SPORTS_KEY;
 }
